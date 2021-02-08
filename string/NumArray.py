8a7d0388e84aa444ec43590c3ffc2b12e22160fd
908jyw <908jyw@gmail.com>
2021-02-08 16:58:22 +0900

    fix : [String] 나누어 떨어지는 숫자 배열 - 프로그래머스 1단계
    완료

--- string/NumArray.py
+++ string/NumArray.py
@@ -1,1 +1,21 @@
 # [String] 나누어 떨어지는 숫자 배열 - 프로그래머스 1단계
+
+arr = [5, 9, 7, 10]
+divisor = 5
+
+def solution(arr, divisor):
+    answer = []
+    for i in arr:
+        if (i % divisor == 0):
+            answer.append(i)
+
+    if (len(answer) == 0):
+        answer.append(-1)
+    else:
+        answer.sort()
+
+    return answer
+
+result = solution(arr, divisor)
+
+print(result)
