1d9afa7b9b33efc8e665234a07dc918dd5c73abd
yw <908jyw@gmail.com>
2021-02-11 19:21:52 +0900

    feat : [BFS,DFS] 미세먼지 안녕! - 백준 17144 삼성기출
    미완료

--- graph/microDust.py
+++ graph/microDust.py
@@ -10,8 +10,6 @@ area = [list(map(int,input().split())) for _ in range(R)]
 increase = [[0] * C for _ in range(R)]
 decrease = [[0] * C for _ in range(R)]
 
-area_bck = copy.deepcopy(area)
-
 dx = [1,0,-1,0]
 dy = [0,1,0,-1]
 
@@ -24,50 +22,32 @@ def spread(y,x):
         ny = y + dy[i]
 
         if(0<= ny <R and 0 <= nx < C):
-            if(area_bck[ny][nx] >= 0):
-                increase[ny][nx] = increase[ny][nx] + (area_bck[y][x] // 5)
+            if(area[ny][nx] >= 0):
+                increase[ny][nx] = increase[ny][nx] + (area[y][x] // 5)
                 cnt += 1
 
-    decrease[y][x] = area_bck[y][x] // 5 * cnt
+    decrease[y][x] = area[y][x] // 5 * cnt
 
 def clean():
     global area_bck
 
     for i in range(1,airClaener[0] - 1):
-        if(area_bck[i-1][0] > 0):
-            area[i][0] = area_bck[i-1][0]
-        else:
-            area[i][0] = 0
+        area[i][0] = area_bck[i-1][0]
 
     for i in range(airClaener[1] + 1 ,R -1):
-        if(area_bck[i + 1][0] > 0):
-            area[i][0] = area_bck[i+1][0]
-        else:
-            area[i][0] = 0
+        area[i][0] = area_bck[i+1][0]
 
     for j in range(0,C-1):
-        if(area_bck[0][j+1] > 0):
-            area[0][j] = area_bck[0][j+1]
-        else:
-            area[0][j] = 0
+        area[0][j] = area_bck[0][j+1]
 
     for j in range(0,C-1):
-        if (area_bck[0][j+1] > 0):
-            area[R-1][j] = area_bck[R-1][j + 1]
-        else:
-            area[R-1][j] = 0
+        area[R-1][j] = area_bck[R-1][j + 1]
 
-    for i in range(airClaener[0] - 1):
-        if(area_bck[i + 1][C-1] > 0):
-            area[i][C - 1] = area_bck[i + 1][C-1]
-        else:
-            area[i][C-1] = 0
+    for i in range(airClaener[0]):
+        area[i][C - 1] = area_bck[i + 1][C-1]
 
     for i in range(airClaener[1]+1, R):
-        if(area_bck[i-1][C-1] > 0):
-            area[i][C - 1] = area_bck[i-1][C-1]
-        else:
-            area[i][C-1] = 0
+        area[i][C - 1] = area_bck[i-1][C-1]
 
     for j in range(1,C):
         if(area_bck[airClaener[0]][j-1] > 0):
@@ -81,16 +61,14 @@ def clean():
         else:
             area[airClaener[1]][j] = 0
 
-    area_bck = copy.deepcopy(area)
-
 airClaener = []
 
 for t in range(T):
     for i in range(R):
         for j in range(C):
-            if(area_bck[i][j] > 0):
+            if(area[i][j] > 0):
                 spread(i,j)
-            if(t == 0 and area_bck[i][j] == -1):
+            if(t == 0 and area[i][j] == -1):
                 airClaener.append(i)
 
 
@@ -98,20 +76,11 @@ for t in range(T):
         for j in range(C):
             area[i][j] = area[i][j] + increase[i][j] - decrease[i][j]
 
-    for i in range(R):
-        print(area[i])
-    print('----')
-
     increase = [[0] * C for _ in range(R)]
     decrease = [[0] * C for _ in range(R)]
 
     area_bck = copy.deepcopy(area)
     clean()
-
-    for i in range(R):
-        print(area[i])
-    print('----')
-
 
 result = 0
 for i in range(R):
