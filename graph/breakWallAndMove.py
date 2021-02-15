5c67921d24fc89ef7b1ffe941b169c343eda011c
yw <908jyw@gmail.com>
2021-02-15 23:42:05 +0900

    feat : [BFS,DFS] 벽 부수고 이동하기 - 백준 2206
    미완료 -> bfs 방식에서 3차원 배열을 이용하여 풀어야 하지만 아직까지 이해하지 못했음

--- graph/breakWallAndMove.py
+++ graph/breakWallAndMove.py
@@ -1,6 +1,5 @@
 # [BFS,DFS] 벽 부수고 이동하기 - 백준 2206
-import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 
 N, M = list(map(int,input().split()))
 
@@ -10,36 +9,73 @@ visited = [[0]*M for _ in range(N)]
 dy = [0,1,0,-1]
 dx = [1,0,-1,0]
 
-result = 10**6
+result = 10 ** 6
+def bfs():
+    q = deque()
+    y = 0
+    x = 0
+    d = 1
+    q.append([y,x,d])
 
-def dfs(y,x,breakYN, cnt):
+    while q:
+        cy,cx,cd = q.popleft()
+        visited[cy][cx] = 1
+        if(cy == N-1 and cx == M-1):
+            global result
+            if(cd < result):
+                result = cd
+            break
 
-    cy = y
-    cx = x
-    cnt += 1
+        for i in range(4):
+            ny = cy + dy[i]
+            nx = cx + dx[i]
 
-    visited[cy][cx] = 1
+            if(0<=ny<N and 0<=nx<M and visited[ny][nx] == 0):
+                if(board[ny][nx] == 0):
+                    q.append([ny,nx,cd+1])
 
-    if(cy == N-1 and cx == M-1):
-        global result
-        if(cnt < result):
-            result = cnt
+for i in range(N):
+    for j in range(M):
+        if(board[i][j] == 1):
+            board[i][j] = 0
+            bfs()
+            board[i][j] = 1
 
-    for i in range(4):
-        ny = cy + dy[i]
-        nx = cx + dx[i]
+bfs()
+if (result == 10**6):
+    print(-1)
+else:
+    print(result)
 
-        if(0<=ny<N and 0<=nx<M and visited[ny][nx] == 0):
-            if(board[ny][nx] == 0):
-                dfs(ny,nx,breakYN,cnt)
-            elif(board[ny][nx] == 1 and breakYN == False):
-                dfs(ny, nx, True, cnt)
-                visited[ny][nx] = 0
 
-dfs(0,0,False,0)
 
-if(result == 10**6):
-    result = -1
-print(result)
-
-print(10**6)
+#
+# result = 10**6
+# def dfs(y,x,breakYN, cnt):
+#
+#     cy = y
+#     cx = x
+#     cnt += 1
+#
+#     visited[cy][cx] = 1
+#
+#     if(cy == N-1 and cx == M-1):
+#         global result
+#         if(cnt < result):
+#             result = cnt
+#
+#     for i in range(4):
+#         ny = cy + dy[i]
+#         nx = cx + dx[i]
+#
+#         if(0<=ny<N and 0<=nx<M and visited[ny][nx] == 0):
+#             if(board[ny][nx] == 0):
+#                 dfs(ny,nx,breakYN,cnt)
+#             elif(board[ny][nx] == 1 and breakYN == False):
+#                 dfs(ny, nx, True, cnt)
+#                 visited[ny][nx] = 0
+#
+# dfs(0,0,False,0)
+#
+#
+# print(result)
