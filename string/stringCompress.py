9b3b48d7337aea23bc314273b26e2d0125c51176
yw <908jyw@gmail.com>
2021-02-26 21:49:35 +0900

    feat : [String] 2020 KAKAO BLIND RECRUITMENT 문자열 압축 - 프로그래머스
    미완료 - 초기 파일 생성 -> 해결법 구상 중

---
+++ string/stringCompress.py
@@ -0,0 +1,26 @@
+# [String] 2020 KAKAO BLIND RECRUITMENT 문자열 압축 - 프로그래머스
+
+
+
+def solution(s):
+    answer = 0
+
+    half = len(s)//2
+
+    print(s[2:4])
+
+
+    for i in range(1, half + 1):
+        first_s = s[0:i]
+        for j in range(0,len(s),i):
+
+
+
+
+    return answer
+
+
+
+s = "aabbaacc"
+
+solution(s)
