362fb9c64b4f0c5e74b58c0c6f99710984d9248f
yw <908jyw@gmail.com>
2021-03-14 18:56:18 +0900

    fix : [String] 모의고사 - 프로그래머스 1단계
    미완료 - 초기값 입력

---
+++ string/mockTest.py
@@ -0,0 +1,21 @@
+# [String] 모의고사 - 프로그래머스 1단계
+
+arr = [1,2,3,4,5]
+
+def solution(answers):
+    answer = []
+
+    student_1 = [1,2,3,4,5]
+    student_2 = [2,1,2,3,2,4,2,5]
+    student_3 = [3,3,1,1,2,2,4,4,5,5]
+
+    result_1 = 0
+
+    for i in range(answers):
+        if(answers[i] == student_1[i]%5):
+
+
+
+    return answer
+
+solution(arr)
