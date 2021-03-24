af40abffb920cca4222824b001e8f920ce4ae939
yw <908jyw@gmail.com>
2021-03-24 23:40:10 +0900

    feat : [Simulation] 어른 상어 - 백준 19237 삼성기출
    미완료 - 테스트케이스 오류 잡는 중

--- simulation/adultShark.py
+++ simulation/adultShark.py
@@ -1,10 +1,13 @@
 # [Simulation] 어른 상어 - 백준 19237 삼성기출
 
+# N*N 격자, M마리의 상어, 흔적 크기 k
 N, M, K = list(map(int,input().split()))
 
 area = []
 for i in range(N):
     area.append(list(map(int,input().split())))
+
+visited = [[0] * N for _ in range(N)]
 
 # 1 위
 # 2 아래
@@ -22,6 +25,7 @@ for i in range(N):
             shark[area[i][j]][0] = i
             shark[area[i][j]][1] = j
             shark[area[i][j]][2] = init_d[area[i][j]-1]
+            visited[i][j] = K
 
 
 
@@ -38,5 +42,121 @@ for i in range(len(init_d)):
 
 print(shark)
 
-for i in range(len(prior_d)):
+for i in range(N):
     print(prior_d[i])
+
+for i in range(len((area))):
+    print(area[i])
+
+print('---')
+
+for i in range(N):
+    print(visited[i])
+
+print('*****')
+
+dy = [0,-1,1,0,0]
+dx = [0,0,0,-1,1]
+
+BOOLEAN = True
+
+def eraseSharkSmell():
+    global BOOLEAN
+    BOOLEAN = False
+    for i in range(N):
+        for j in range(N):
+            # 상어의 크기가 2이상인 상어가 있으면 종료되지 않으므로 boolean 값으로 구분
+            if(area[i][j] == 2):
+                BOOLEAN = True
+            if(visited[i][j] == 0):
+                continue
+            if(visited[i][j] > 0 and area[i][j] == 0):
+                visited[i][j] = visited[i][j] - 1
+
+    print('---')
+    for i in range(N):
+        print(visited[i])
+
+def sharkMove():
+
+    for i in range(M,0,-1):
+
+        cy, cx, d = shark[i]
+
+        if(d == 0):
+            continue
+
+        for j in range(4):
+            nd = prior_d[i][d-1][j]
+
+            ny = cy + dy[nd]
+            nx = cx + dx[nd]
+
+            if(ny<0 or ny>=N or nx<0 or nx>=N):
+                continue
+
+
+            # 이동을 한곳에 나보다 큰 애가 있으면 그 큰 상어는 먹힌다
+            if (area[ny][nx] != 0 and area[ny][nx] > i):
+                shark[area[ny][nx]][0] = 0
+                shark[area[ny][nx]][1] = 0
+                shark[area[ny][nx]][2] = 0
+                visited[ny][nx] = 0
+
+            if(j==3 and visited[ny][nx] != 0):
+                if(d == 1):
+                    nd = 2
+                if(d == 2):
+                    nd = 1
+                if(d == 3):
+                    nd = 4
+                if(d == 4):
+                    nd = 3
+                ny = cy + dy[nd]
+                nx = cx + dx[nd]
+
+
+            if(j != 3 and visited[ny][nx] != 0):
+                continue
+
+            # 이동을 한곳에 나보다 큰 애가 있으면 그 큰 상어는 먹힌다
+            # if(area[ny][nx] != 0 and area[ny][nx] > i):
+            #     shark[area[ny][nx]][0] = 0
+            #     shark[area[ny][nx]][1] = 0
+            #     shark[area[ny][nx]][2] = 0
+
+            # 상어 이동 후 공간은 상어가 없음
+            area[cy][cx] = 0
+            # 상어 이동 후 새로운 공간은 현재 상어가 존재
+            area[ny][nx] = i
+            # 이동한 칸의 초기 향기는 K
+            visited[ny][nx] = K
+            # 상어 좌표 변경
+            shark[i][0] = ny
+            shark[i][1] = nx
+            shark[i][2] = nd
+
+            break
+
+
+    print('---')
+    for i in range(len((area))):
+        print(area[i])
+    print('---')
+    for i in range(M + 1):
+        print(shark[i][2],end=' ')
+    eraseSharkSmell()
+
+
+result = 0
+while BOOLEAN == True:
+    if(result>1000):
+        break
+    sharkMove()
+    result += 1
+    print('result = ',result)
+
+if(result>1000):
+    print(-1)
+else:
+    print(result)
