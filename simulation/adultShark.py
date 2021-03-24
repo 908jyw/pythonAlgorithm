22ab855607742297c4dab811dfcca690f0e8bae2
yw <908jyw@gmail.com>
2021-03-25 01:31:27 +0900

    [Simulation] 어른 상어 - 백준 19237 삼성기출
    미완료 - 예외케이스 찾아야함

--- simulation/adultShark.py
+++ simulation/adultShark.py
@@ -1,5 +1,7 @@
 # [Simulation] 어른 상어 - 백준 19237 삼성기출
 
+import copy
+
 # N*N 격자, M마리의 상어, 흔적 크기 k
 N, M, K = list(map(int,input().split()))
 
@@ -7,7 +9,11 @@ area = []
 for i in range(N):
     area.append(list(map(int,input().split())))
 
-visited = [[0] * N for _ in range(N)]
+
+visited = [[[0] * 2 for _ in range(N)] for _ in range(N)]
+# visited = [[0,0] * N for _ in range(N)]
+
+
 
 # 1 위
 # 2 아래
@@ -25,7 +31,8 @@ for i in range(N):
             shark[area[i][j]][0] = i
             shark[area[i][j]][1] = j
             shark[area[i][j]][2] = init_d[area[i][j]-1]
-            visited[i][j] = K
+            visited[i][j][0] = area[i][j]
+            visited[i][j][1] = K
 
 
 
@@ -42,7 +49,7 @@ for i in range(len(init_d)):
 
 print(shark)
 
-for i in range(N):
+for i in range(M+1):
     print(prior_d[i])
 
 for i in range(len((area))):
@@ -68,16 +75,20 @@ def eraseSharkSmell():
             # 상어의 크기가 2이상인 상어가 있으면 종료되지 않으므로 boolean 값으로 구분
             if(area[i][j] == 2):
                 BOOLEAN = True
-            if(visited[i][j] == 0):
+            if(visited[i][j][1] == 0):
                 continue
-            if(visited[i][j] > 0 and area[i][j] == 0):
-                visited[i][j] = visited[i][j] - 1
+            if(visited[i][j][1] > 0 and area[i][j] == 0):
+                visited[i][j][1] = visited[i][j][1] - 1
+                if(visited[i][j][1] == 0):
+                    visited[i][j][0] = 0
 
     print('---')
     for i in range(N):
         print(visited[i])
 
 def sharkMove():
+
+    visited_copy = copy.deepcopy(visited)
 
     for i in range(M,0,-1):
 
@@ -97,40 +108,41 @@ def sharkMove():
 
 
             # 이동을 한곳에 나보다 큰 애가 있으면 그 큰 상어는 먹힌다
-            if (area[ny][nx] != 0 and area[ny][nx] > i):
+            if (area[ny][nx] != 0 and area[ny][nx] > i and visited[ny][nx][1] != 0 and visited_copy[ny][nx][1] == 0):
                 shark[area[ny][nx]][0] = 0
                 shark[area[ny][nx]][1] = 0
                 shark[area[ny][nx]][2] = 0
-                visited[ny][nx] = 0
+                visited[ny][nx][0] = 0
+                visited[ny][nx][1] = 0
 
-            if(j==3 and visited[ny][nx] != 0):
-                if(d == 1):
-                    nd = 2
-                if(d == 2):
-                    nd = 1
-                if(d == 3):
-                    nd = 4
-                if(d == 4):
-                    nd = 3
-                ny = cy + dy[nd]
-                nx = cx + dx[nd]
+            # for 문을 돌았는데 다음으로 갈 공간이 없으면 내가 남긴 흔적 방향으로 가야됨
+            if(j==3 and visited[ny][nx][1] != 0):
+                for k in range(4):
+                    nd = prior_d[i][d - 1][k]
+                    ny = cy + dy[nd]
+                    nx = cx + dx[nd]
 
+                    if (ny < 0 or ny >= N or nx < 0 or nx >= N):
+                        continue
 
-            if(j != 3 and visited[ny][nx] != 0):
+                    if(visited[ny][nx][0] == i):
+                        #shark[area[ny][nx]][0] = 0
+                        #shark[area[ny][nx]][1] = 0
+                        #shark[area[ny][nx]][2] = 0
+                        #visited[ny][nx][0] = i
+                        #visited[ny][nx][1] = k
+                        break
+
+            if(j != 3 and visited[ny][nx][0] != 0):
                 continue
-
-            # 이동을 한곳에 나보다 큰 애가 있으면 그 큰 상어는 먹힌다
-            # if(area[ny][nx] != 0 and area[ny][nx] > i):
-            #     shark[area[ny][nx]][0] = 0
-            #     shark[area[ny][nx]][1] = 0
-            #     shark[area[ny][nx]][2] = 0
 
             # 상어 이동 후 공간은 상어가 없음
             area[cy][cx] = 0
             # 상어 이동 후 새로운 공간은 현재 상어가 존재
             area[ny][nx] = i
             # 이동한 칸의 초기 향기는 K
-            visited[ny][nx] = K
+            visited[ny][nx][0] = i
+            visited[ny][nx][1] = K
             # 상어 좌표 변경
             shark[i][0] = ny
             shark[i][1] = nx
