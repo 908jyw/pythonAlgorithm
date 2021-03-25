08f4d4364d6e4ece50e339d9d9f123a8354ef901
yw <908jyw@gmail.com>
2021-03-25 23:43:25 +0900

    fix : [Simulation] 어른 상어 - 백준 19237 삼성기출
    완료 - 코드 리뷰하기

--- simulation/adultShark.py
+++ simulation/adultShark.py
@@ -35,11 +35,6 @@ for i in range(N):
             visited[i][j][1] = K
 
 
-
-
-# prior_d = [[[0] * 4 for _ in range(4)] for _ in range(len(init_d) +1)]
-# prior_d = [[[0] * 4 for _ in range(4)] for _ in range(len(init_d) +1)]
-
 prior_d = [[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]]
 for i in range(len(init_d)):
     li = []
@@ -47,20 +42,20 @@ for i in range(len(init_d)):
         li.append(list(map(int,input().split())))
     prior_d.append(li)
 
-print(shark)
-
-for i in range(M+1):
-    print(prior_d[i])
-
-for i in range(len((area))):
-    print(area[i])
-
-print('---')
-
-for i in range(N):
-    print(visited[i])
-
-print('*****')
+# print(shark)
+#
+# for i in range(M+1):
+#     print(prior_d[i])
+#
+# for i in range(len((area))):
+#     print(area[i])
+#
+# print('---')
+#
+# for i in range(N):
+#     print(visited[i])
+#
+# print('*****')
 
 dy = [0,-1,1,0,0]
 dx = [0,0,0,-1,1]
@@ -73,7 +68,7 @@ def eraseSharkSmell():
     for i in range(N):
         for j in range(N):
             # 상어의 크기가 2이상인 상어가 있으면 종료되지 않으므로 boolean 값으로 구분
-            if(area[i][j] == 2):
+            if(area[i][j] >= 2):
                 BOOLEAN = True
             if(visited[i][j][1] == 0):
                 continue
@@ -82,15 +77,16 @@ def eraseSharkSmell():
                 if(visited[i][j][1] == 0):
                     visited[i][j][0] = 0
 
-    print('---')
-    for i in range(N):
-        print(visited[i])
+    # print('---')
+    # for i in range(N):
+    #     print(visited[i])
 
 def sharkMove():
 
     visited_copy = copy.deepcopy(visited)
 
     for i in range(M,0,-1):
+        move = False
 
         cy, cx, d = shark[i]
 
@@ -106,35 +102,36 @@ def sharkMove():
             if(ny<0 or ny>=N or nx<0 or nx>=N):
                 continue
 
+            # for 문을 돌았는데 다음으로 갈 공간이 없으면 내가 남긴 흔적 방향으로 가야됨
+            # 따라서 for문의 끝인데, 다음 갈 공간이 내 흔적도 아니면
+            # if(j==3 and visited_copy[ny][nx][1] != i and ):
+            elif (j == 3 and visited_copy[ny][nx][1] != 0):
+                for k in range(4):
+                    nd = prior_d[i][d - 1][k]
+                    ny = cy + dy[nd]
+                    nx = cx + dx[nd]
+                    # if (i == 1): print('33', ny, nx)
+                    if (ny < 0 or ny >= N or nx < 0 or nx >= N):
+                        continue
+
+                    if (visited_copy[ny][nx][0] == i):
+
+                        break
             # 일단 이동못하는 경우는? 내가 방문하려는 곳에 이미 흔적이 있으면 못가
-            if (visited_copy[ny][nx][0] != 0):
+            elif (visited_copy[ny][nx][0] != 0):
+                # if(i == 1) : print('11',ny,nx)
                 continue
 
-            # 이동을 한곳에 나보다 큰 애가 있으면 그 큰 상어는 먹힌다
-            if (area[ny][nx] != 0 and area[ny][nx] > i and visited_copy[ny][nx][0] == 0):
+            # 이동을 한곳에 나보다 큰 애가 있으면 그 큰 상어는 먹힌다 여기서 갈곳의 상태가 흔적이 없는 상태여야지만 가
+            elif (area[ny][nx] != 0 and area[ny][nx] > i and visited_copy[ny][nx][0] == 0):
+                # if (i == 1): print('22',ny, nx)
                 shark[area[ny][nx]][0] = 0
                 shark[area[ny][nx]][1] = 0
                 shark[area[ny][nx]][2] = 0
                 #visited[ny][nx][0] = 0
                 #visited[ny][nx][1] = 0
 
-
-
-            # for 문을 돌았는데 다음으로 갈 공간이 없으면 내가 남긴 흔적 방향으로 가야됨
-            # 따라서 for문의 끝인데, 다음 갈 공간이 내 흔적도 아니면
-            if(j==3 and visited_copy[ny][nx][1] != i):
-                for k in range(4):
-                    nd = prior_d[i][d - 1][k]
-                    ny = cy + dy[nd]
-                    nx = cx + dx[nd]
-
-                    if (ny < 0 or ny >= N or nx < 0 or nx >= N):
-                        continue
-
-                    if(visited_copy[ny][nx][0] == i):
-                        break
-
-            if(j != 3 and visited[ny][nx][0] != 0):
+            elif(j != 3 and visited_copy[ny][nx][0] != 0):
                 continue
 
             # 상어 이동 후 공간은 상어가 없음
@@ -149,15 +146,44 @@ def sharkMove():
             shark[i][1] = nx
             shark[i][2] = nd
 
+            move = True
+
             break
 
+        if(move == False):
+            for j in range(4):
+                nd = prior_d[i][d - 1][j]
 
-    print('---')
-    for i in range(len((area))):
-        print(area[i])
-    print('---')
-    for i in range(M + 1):
-        print(shark[i][2],end=' ')
+                ny = cy + dy[nd]
+                nx = cx + dx[nd]
+
+                if (ny < 0 or ny >= N or nx < 0 or nx >= N):
+                    continue
+                if (visited_copy[ny][nx][0] == i):
+                    break
+
+            # 상어 이동 후 공간은 상어가 없음
+            area[cy][cx] = 0
+            # 상어 이동 후 새로운 공간은 현재 상어가 존재
+            area[ny][nx] = i
+            # 이동한 칸의 초기 향기는 K
+            visited[ny][nx][0] = i
+            visited[ny][nx][1] = K
+            # 상어 좌표 변경
+            shark[i][0] = ny
+            shark[i][1] = nx
+            shark[i][2] = nd
+
+
+
+
+
+    # print('---')
+    # for i in range(len((area))):
+    #     print(area[i])
+    # print('---')
+    # for i in range(M + 1):
+    #     print(shark[i][2],end=' ')
     eraseSharkSmell()
 
 
@@ -167,7 +193,7 @@ while BOOLEAN == True:
         break
     sharkMove()
     result += 1
-    print('result = ',result)
+    # print('result = ',result)
 
 if(result>1000):
     print(-1)
