857686d4a29a7c785b9cdba2973ac5eec796f4a2
yw <908jyw@gmail.com>
2021-04-05 01:14:55 +0900

    fix : [BFS,DFS] 스타트택시 - 백준 19238 삼성기출
    완료

--- graph/startTaxi.py
+++ graph/startTaxi.py
@@ -22,14 +22,14 @@ for i in range(M):
     area[cy][cx] = 9
 
 
-print('공간')
-for i in range(N+1):
-    print(area[i])
-print('공간끝')
-
-print('택시')
-print(taxi)
-print('택시끝')
+# print('공간')
+# for i in range(N+1):
+#     print(area[i])
+# print('공간끝')
+#
+# print('택시')
+# print(taxi)
+# print('택시끝')
 
 
 
@@ -40,6 +40,7 @@ dx = [0,-1,1,0]
 def dfs_after(y,x,f,fy,fx):
     visited_after = [[0]*(N+1) for _ in range(N+1)]
 
+    # print(y,x,f,fy,fx)
     q = deque()
     visited_after[y][x] = 1
     q.append([y,x,f])
@@ -54,6 +55,8 @@ def dfs_after(y,x,f,fy,fx):
         # 목적지 도착했으면
         if(cy==fy and cx==fx):
             cf = cf + (visited_after[cy][cx]-1) * 2
+            q.clear()
+            # print('dfs_after = ', cy,cx,cf)
             return [cy,cx,cf]
 
         for i in range(4):
@@ -70,12 +73,12 @@ def dfs_after(y,x,f,fy,fx):
                 visited_after[ny][nx] = visited_after[cy][cx] + 1
                 q.append([ny,nx,nf])
 
-        print('목적지로 가는 bfs')
-        for j in range(len(visited_after)):
-            print(visited_after[j])
-        print('목적지로 가는 bfs 끝')
+        # print('목적지로 가는 bfs')
+        # for j in range(len(visited_after)):
+        #     print(visited_after[j])
+        # print('목적지로 가는 bfs 끝')
 
-
+    return [-1,-1,-1]
 
 
 def dfs_before(y,x,f):
@@ -91,17 +94,19 @@ def dfs_before(y,x,f):
         nf = cf - 1
 
         # 사람 태우러 가는 길에 연료 바닥이면 -1리턴
-        if(nf == 0):
-            return -1
+        if(cf == 0):
+            return [-1,-1,-1]
 
         # 현재 위치에 사람이 있으면 태움
         if(area[cy][cx] == 9):
             for i in range(len(people)):
                 people_y,people_x,people_ny,people_nx = people[i]
                 if(people_y == cy and people_x == cx):
+                    area[cy][cx] = 0
                     new_y,new_x,result_f = dfs_after(cy,cx,cf,people_ny,people_nx)
+                    hq.clear()
+                    # print('dfs_before = ', new_y,new_x,result_f)
                     return [new_y,new_x,result_f]
-
             break
 
 
@@ -117,15 +122,22 @@ def dfs_before(y,x,f):
                 visited_before[ny][nx] = visited_before[cy][cx] + 1
                 heapq.heappush(hq,[nd,ny,nx,nf])
 
-        print('승객 태우러 가는 bfs')
-        for j in range(len(visited_before)):
-            print(visited_before[j])
-        print('승객 태우러 가는 bfs 끝')
+        # print('승객 태우러 가는 bfs')
+        # for j in range(len(visited_before)):
+        #     print(visited_before[j])
+        # print('승객 태우러 가는 bfs 끝')
+    return [-1,-1,-1]
 
-new_y,new_x,result_f = taxi[0],taxi[1],F
 
-while result_f > 0:
+new_y = taxi[0]
+new_x = taxi[1]
+result_f = F
+
+
+while (result_f > 0 and M > 0):
     new_y,new_x,result_f = dfs_before(new_y,new_x,result_f)
+    M = M-1
 
+#new_y,new_x,result_f = dfs_before(new_y,new_x,result_f)
 
 print(result_f)
