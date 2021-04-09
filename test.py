05caeb6156b334f5c8a3bb980e425ffb121828f4
908jyw <908jyw@gmail.com>
2021-04-09 17:29:21 +0900

    feat : test 파일 동기화

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
 # 테스트 파이썬 파일
 
-visited = [[[0] * 2 for _ in range(5)] for _ in range(5)]
+# visited = [[[0] * 2 for _ in range(5)] for _ in range(5)]
+#
+# for i in range(5):
+#     print(visited[i])
 
-for i in range(5):
-    print(visited[i])
+
+
+a = 0
+b = 10
+
+result = a % b
+
+print(result)
+
+
+print(5 % 2)
+print(-5 % -2)
+print(5 % -2)
+print(-5 %  2)
+
