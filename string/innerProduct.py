f377b77b8ce147d60da268c9f11b5f7e0b5da7cf
yw <908jyw@gmail.com>
2021-04-26 22:57:04 +0900

    fix : [String] 내적 - 프로그래머스 1단계
    완료

--- string/innerProduct.py
+++ string/innerProduct.py
@@ -1,3 +1,15 @@
 # [String] 내적 - 프로그래머스 1단계
 
-N = int(input())
+def solution(a, b):
+    answer = 0
+
+    for i in range(len(a)):
+        answer = answer + a[i]*b[i]
+
+    return answer
+
+
+a = [1,2,3,4]
+b = [-3,-1,0,2]
+
+print(solution(a,b))
