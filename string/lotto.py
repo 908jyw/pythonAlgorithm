eb5de67e9e246729af48dd264c6873fdb9dc0db0
yw <908jyw@gmail.com>
2021-04-29 22:10:58 +0900

    feat : [String] 로또의 최고 순위와 최저 순위 - 프로그래머스 1단계
    미완료 - 입력값 구현

--- string/lotto.py
+++ string/lotto.py
@@ -2,7 +2,6 @@
 
 def solution(lottos, win_nums):
     answer = []
-
     cnt = 0
     for i in win_nums:
         if(i in lottos):
@@ -13,3 +12,6 @@ def solution(lottos, win_nums):
 
 lottos = [44, 1, 0, 0, 31, 25]
 win_nums = [31, 10, 45, 1, 6, 19]
+
+
+print(solution(lottos,win_nums))
