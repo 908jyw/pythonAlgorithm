688220d4288a0bbe14c38fc98ae365295075c24d
908jyw <908jyw@gmail.com>
2021-05-20 21:51:09 +0900

    feat : itertools 사용 (permutations, combinations)

--- backtracking/nAndM2.py
+++ backtracking/nAndM2.py
@@ -18,33 +18,45 @@
 # dfs(0, 1)
 
 
-
-
-
+import itertools
 import sys
-import copy
 
-N,M = list(map(int,sys.stdin.readline().split()))
+N, M = list(map(int,sys.stdin.readline().split()))
 
-visited = [0] * (N+1)
-result = [0] * (M+1)
-
-def dfs(depth,num,visited):
-    visited_copy = copy.deepcopy(visited)
-    visited_copy[num] = 1
-
-    if(depth < M):
-        result[depth] = num
-        for i in range(num+1,N+1):
-            if(visited_copy[i] == 0):
-                dfs(depth+1,i,visited_copy)
-    else:
-        result[depth] = num
-        for i in range(1,M+1):
-            print(result[i],end=' ')
-        print()
-
-
-
+num = []
 for i in range(1,N+1):
-    dfs(1,i,visited)
+    num.append(str(i))
+
+result = list(map(' '.join,itertools.combinations(num,M)))
+for i in range(len(result)):
+    print(result[i])
+
+
+
+# import sys
+# import copy
+#
+# N,M = list(map(int,sys.stdin.readline().split()))
+#
+# visited = [0] * (N+1)
+# result = [0] * (M+1)
+#
+# def dfs(depth,num,visited):
+#     visited_copy = copy.deepcopy(visited)
+#     visited_copy[num] = 1
+#
+#     if(depth < M):
+#         result[depth] = num
+#         for i in range(num+1,N+1):
+#             if(visited_copy[i] == 0):
+#                 dfs(depth+1,i,visited_copy)
+#     else:
+#         result[depth] = num
+#         for i in range(1,M+1):
+#             print(result[i],end=' ')
+#         print()
+#
+#
+#
+# for i in range(1,N+1):
+#     dfs(1,i,visited)
