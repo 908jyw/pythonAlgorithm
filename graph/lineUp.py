5e9f97ce238365bb15e7292d75e71e45264a6072
yw <908jyw@gmail.com>
2021-05-26 23:57:32 +0900

    feat : 위상정렬 참고

--- graph/lineUp.py
+++ graph/lineUp.py
@@ -20,11 +20,13 @@ def topologySort(node,nodeLine):
     q = deque()
     result = []
 
+    # 진입차수가 0인 노드를 큐에 삽입
     for i in range(1,N+1):
         if(nodeLine[i] == 0):
             result.append(i)
             q.append(i)
 
+    # 위상 정렬이 완전히 수행되려면 정확히 N개의 노드를 방문, 중간에 q가 null 이 되면 싸이클이 존재한는 것이다.
     for i in range(1,N+1):
         x = q.popleft()
         #print('node = ', len(node[x]))
