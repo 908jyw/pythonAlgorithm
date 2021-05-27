e31d5992e1dc40fd0e138a1352f23e622aea88fe
908jyw <908jyw@gmail.com>
2021-05-27 13:12:14 +0900

    feat : 코딩테스트 준비를 위한 주요 문제 다시풀기

--- graph/findTheLeastExpense.py
+++ graph/findTheLeastExpense.py
@@ -2,53 +2,104 @@
 
 import heapq
 
-N = int(input())
-M = int(input())
+# N = int(input())
+# M = int(input())
+#
+# node = [[] for _ in range(N + 1)]
+#
+# INF = 10000000000
+#
+# # 시작점에서 각 노드까지의 거리를 저장할 변수
+# distance = [INF] * (N+1)
+# # 이미 방문한 노드의 연산을 줄이기 위해 생성한 방문 여부 변수
+# visited = [0] * (N+1)
+#
+# # 반복문을 통해서 각 노드의 연결된 노드와 거리를 변수에 저장
+# for i in range(M):
+#     a,b,d = list(map(int,input().split()))
+#     node[a].append([b,d])
+#
+# start, end = list(map(int,input().split()))
+#
+# # 출발점을 매개변수로 가장 짧은 거리를 구하는 다익스트라 알고리즘 사용
+# def dijkstra(start):
+#     distance[start] = 0
+#     hq = []
+#     visited[start] = 1
+#
+#     # 시작 노드와 연결된 노드의 정보를 우선순위 큐에 담는다다
+#     for i in node[start]:
+#         b = i[0]
+#         d = i[1]
+#         if(distance[b] > d):
+#             distance[b] = d
+#         # 가장 짧은 거리부터 반환하기 위해 우선순위 큐 사용
+#         heapq.heappush(hq,[d,b])
+#
+#     while hq:
+#         d, b = heapq.heappop(hq)
+#         # 이미 방문한 노드면 연산 필요 없음
+#         if(visited[b] == 1):
+#             continue
+#         # 현재 노드를 방문 처리
+#         visited[b] = 1
+#         # 현재 노드에서 연결된 노드의 정보를 통해 최소 거리를 갱신하고, 우선순위 큐에 담는
+#         for j in node[b]:
+#             nb, nd = j[0], j[1]
+#             if(distance[nb] > distance[b] + nd):
+#                 distance[nb] = distance[b] + nd
+#                 heapq.heappush(hq,[distance[nb],nb])
+#
+#
+# dijkstra(start)
+#
+# print(distance[end])
 
-node = [[] for _ in range(N + 1)]
 
-INF = 10000000000
+import sys
 
-# 시작점에서 각 노드까지의 거리를 저장할 변수
-distance = [INF] * (N+1)
-# 이미 방문한 노드의 연산을 줄이기 위해 생성한 방문 여부 변수
-visited = [0] * (N+1)
+# 다시풀기
 
-# 반복문을 통해서 각 노드의 연결된 노드와 거리를 변수에 저장
+N = int(sys.stdin.readline().strip())
+M = int(sys.stdin.readline().strip())
+
+INF = 10**9
+distance = [INF] * (N + 1)
+visited = [0] * (N + 1)
+
+city = [[] for _ in range(N+1)]
+
 for i in range(M):
-    a,b,d = list(map(int,input().split()))
-    node[a].append([b,d])
+    a,b,d = list(map(int,sys.stdin.readline().split()))
+    city[a].append([b,d])
 
-start, end = list(map(int,input().split()))
+start, end = list(map(int,sys.stdin.readline().split()))
 
-# 출발점을 매개변수로 가장 짧은 거리를 구하는 다익스트라 알고리즘 사용
+#print(city)
+
 def dijkstra(start):
     distance[start] = 0
     hq = []
     visited[start] = 1
-
-    # 시작 노드와 연결된 노드의 정보를 우선순위 큐에 담는다다
-    for i in node[start]:
-        b = i[0]
-        d = i[1]
+    for i in city[start]:
+        b,d = i[0], i[1]
         if(distance[b] > d):
             distance[b] = d
-        # 가장 짧은 거리부터 반환하기 위해 우선순위 큐 사용
         heapq.heappush(hq,[d,b])
 
     while hq:
-        d, b = heapq.heappop(hq)
-        # 이미 방문한 노드면 연산 필요 없음
-        if(visited[b] == 1):
+        d,b = heapq.heappop(hq)
+        if(len(city[b]) == 0):
             continue
-        # 현재 노드를 방문 처리
-        visited[b] = 1
-        # 현재 노드에서 연결된 노드의 정보를 통해 최소 거리를 갱신하고, 우선순위 큐에 담는
-        for j in node[b]:
-            nb, nd = j[0], j[1]
+        for i in city[b]:
+            nb,nd = i[0], i[1]
+            if(visited[nb] > 0):
+                continue
+
             if(distance[nb] > distance[b] + nd):
                 distance[nb] = distance[b] + nd
-                heapq.heappush(hq,[distance[nb],nb])
+                heapq.heappush(hq,[nd,nb])
+    return 0
 
 
 dijkstra(start)
--- graph/lineUp.py
+++ graph/lineUp.py
@@ -3,43 +3,82 @@
 import sys
 from collections import deque
 
-N,M = list(map(int,sys.stdin.readline().split()))
+# N,M = list(map(int,sys.stdin.readline().split()))
+#
+# nodeLine = [0] * (N+1)
+# node = [[] for _ in range(N+1)]
+#
+# for i in range(M):
+#     a,b = list(map(int,sys.stdin.readline().split()))
+#     node[a].append(b)
+#     nodeLine[b] += 1
+#
+#
+#
+# def topologySort(node,nodeLine):
+#W
+#     q = deque()
+#     result = []
+#
+#     # 진입차수가 0인 노드를 큐에 삽입
+#     for i in range(1,N+1):
+#         if(nodeLine[i] == 0):
+#             result.append(i)
+#             q.append(i)
+#
+#     # 위상 정렬이 완전히 수행되려면 정확히 N개의 노드를 방문, 중간에 q가 null 이 되면 싸이클이 존재한는 것이다.
+#     for i in range(1,N+1):
+#         x = q.popleft()
+#         #print('node = ', len(node[x]))
+#         if(len(node[x]) > 0):
+#             for j in node[x]:
+#                 nodeLine[j] -= 1
+#                 if(nodeLine[j] == 0):
+#                     result.append(j)
+#                     q.append(j)
+#
+#     return result
+#
+# result = topologySort(node,nodeLine)
+#
+# for i in result:
+#     print(i,end=' ')
 
-nodeLine = [0] * (N+1)
+
+# 다시 풀어보기
+N, M = list(map(int,sys.stdin.readline().split()))
+
 node = [[] for _ in range(N+1)]
+result = []
+node_line_num = [0] * (N+1)
 
 for i in range(M):
-    a,b = list(map(int,sys.stdin.readline().split()))
+    a, b = list(map(int,sys.stdin.readline().split()))
     node[a].append(b)
-    nodeLine[b] += 1
+    node_line_num[b] += 1
 
 
-
-def topologySort(node,nodeLine):
-
+def toporogySort(node,node_line_num):
     q = deque()
-    result = []
-
-    # 진입차수가 0인 노드를 큐에 삽입
-    for i in range(1,N+1):
-        if(nodeLine[i] == 0):
-            result.append(i)
+    for i in range(1,len(node_line_num)):
+        if(node_line_num[i]==0):
             q.append(i)
 
-    # 위상 정렬이 완전히 수행되려면 정확히 N개의 노드를 방문, 중간에 q가 null 이 되면 싸이클이 존재한는 것이다.
-    for i in range(1,N+1):
-        x = q.popleft()
-        #print('node = ', len(node[x]))
-        if(len(node[x]) > 0):
-            for j in node[x]:
-                nodeLine[j] -= 1
-                if(nodeLine[j] == 0):
-                    result.append(j)
-                    q.append(j)
+    for i in range(1,len(node)):
+        n = q.popleft()
+        result.append(n)
+
+        if(len(node[n]) == 0):
+            continue
+
+        for j in range(len(node[n])):
+            node_line_num[node[n][j]] -= 1
+            if(node_line_num[node[n][j]] == 0):
+                q.append(node[n][j])
 
     return result
 
-result = topologySort(node,nodeLine)
+result = toporogySort(node,node_line_num)
 
 for i in result:
     print(i,end=' ')
