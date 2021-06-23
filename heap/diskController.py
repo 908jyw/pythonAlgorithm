e7c87ef831f52e5d632f06b4c034aa221137e67d
yw <908jyw@gmail.com>
2021-06-23 23:55:14 +0900

    feat : 디스크컨트롤러 추가

---
+++ heap/diskController.py
@@ -0,0 +1,21 @@
+import heapq
+
+
+def solution(jobs):
+    answer = 0
+
+    time = 0
+
+    hq = []
+    finish_time = []
+    for i in jobs:
+        heapq.heqppush(hq, i)
+
+
+
+jobs = [[0, 3], [2, 6], [1, 9]]
+
+
+print(jobs.sort())
+
+#solution(jobs)
